e0714fa34e36318aea84170685e859d7866f741c
clegand <corentinlgnd@gmail.com>
2026-04-23 14:49:23 +0200

    add user struct & interface, install uuid google

--- backend/model/user_model.go
+++ backend/model/user_model.go
@@ -1,1 +1,30 @@
 package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	FirstName string
+	LastName  string
+	NickName  string `gorm:"uniqueIndex"`
+	Birthday  string
+	Email     string `gorm:"uniqueIndex"`
+	Password  string
+	Avatar    string
+	AboutMe   *string
+	CreatedAt time.Time
+	UpdateAt  time.Time
+}
+
+type UserRepository interface {
+	Create(user *User) error
+	GetAll() ([]User, error)
+	GetByNickName(nickname string) (*User, error)
+	GetByID(id uuid.UUID) (*User, error)
+	Delete(id uuid.UUID) error
+	Update(user *User) error
+}

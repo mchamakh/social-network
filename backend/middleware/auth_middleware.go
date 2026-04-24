679a191f902ecc5ec5b1337afa093a2fbb73a417
clegand <corentinlgnd@gmail.com>
2026-04-24 19:43:50 +0200

    add middleware for auth

--- backend/middleware/auth_middleware.go
+++ backend/middleware/auth_middleware.go
@@ -1,1 +1,30 @@
 package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		authHeader := c.GetHeader("Authorization")
+
+		if authHeader == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "missing token"})
+			return
+		}
+
+		token := strings.TrimPrefix(authHeader, "Bearer ")
+
+		if token == authHeader {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token format"})
+			return
+		}
+
+		c.Set("access_token", token)
+		c.Next()
+	}
+}

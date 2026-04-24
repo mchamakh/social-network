01b3d616c32dc5f7923e5941560993004f493dd2
mchamakh <myriamchmkh@gmail.com>
2026-04-24 10:01:18 +0200

    import new font for title

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -16,10 +16,10 @@ export default function Home() {
         </div>
       </nav>
       <div className="flex flex-col items-center justify-center mt-48 gap-4 text-center">
-        <h1 className="text-6xl text-black font-serif">
-          Welcome on <span className="text-blue-300">Social</span>
+        <h1 className="text-6xl text-black font-(family-name:--font-domine)">
+          Welcome on <span className="text-blue-300 blur-[2px]">Social</span>
         </h1>
-        <p className="text-gray-500 text-lg">
+        <p className=" text-gray-500 text-lg">
           Connect, share, and discover people who are just like you.
         </p>
         <button className="mt-4 bg-black text-white px-8 py-3 rounded-full font-medium hover:bg-zinc-800 transition-colors">

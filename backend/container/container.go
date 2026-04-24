aed88bbd7308327922c4a0e0af32d1c3579d55dd
clegand <corentinlgnd@gmail.com>
2026-04-24 15:20:08 +0200

    add auth handler for container

--- backend/container/container.go
+++ backend/container/container.go
@@ -10,14 +10,21 @@ import (
 
 type Container struct {
 	UserHandler *handler.UserHandler
+	AuthHandler *handler.AuthHandler
 }
 
 func NewContainer(db *gorm.DB) *Container {
 	userRepo := repository.NewUserRepository(db)
+	refreshRepo := repository.NewRefreshTokenRepository(db)
+
 	userService := service.NewUserService(userRepo)
+	authService := service.NewAuthService(userService, refreshRepo)
+
 	userHandler := handler.NewUserHandler(userService)
+	authHandler := handler.NewAuthHandler(authService)
 
 	return &Container{
 		UserHandler: userHandler,
+		AuthHandler: authHandler,
 	}
 }

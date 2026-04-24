4dfb8d8d9ce075877542a519021b47fb1084f475
clegand <corentinlgnd@gmail.com>
2026-04-24 12:02:08 +0200

    add hash password & compare password logic with bcrypt

--- backend/pkg/hash.go
+++ backend/pkg/hash.go
@@ -1,1 +1,12 @@
 package pkg
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+func ComparePassword(password, hash string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}

f60c06466056462f5f551d24014fbb2aae14e3fd
clegand <corentinlgnd@gmail.com>
2026-04-24 15:27:18 +0200

    add tokens model

---
+++ backend/model/refresh_token_model.go
@@ -0,0 +1,21 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type RefreshToken struct {
+	ID        uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	UserID    uuid.UUID `gorm:"type:uuid;not null"`
+	Token     string    `gorm:"not null;uniqueIndex"`
+	ExpiresAt time.Time `gorm:"not null"`
+	CreatedAt time.Time
+}
+
+type RefreshTokenRepository interface {
+	Create(userID uuid.UUID, token string) error
+	Find(token string) (*RefreshToken, error)
+	Delete(token string) error
+}

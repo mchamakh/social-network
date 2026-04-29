30ace63a00127c23eb15e81907fde94f17802b87
mchamakh <myriamchmkh@gmail.com>
2026-04-29 11:13:48 +0200

    create sidebar component

---
+++ frontend/app/profile/page.tsx
@@ -0,0 +1,9 @@
+import Sidebar from "@/components/Sidebar";
+
+export default function Profile() {
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      <Sidebar />
+    </div>
+  );
+}

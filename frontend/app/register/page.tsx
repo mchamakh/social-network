e96536b228daf1b2033159340bb3b8c570ec722a
mchamakh <myriamchmkh@gmail.com>
2026-04-29 09:12:46 +0200

    add required field & condition for next step

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -1,9 +1,21 @@
 "use client";
 import { useState } from "react";
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+import Link from "next/link";
 
 export default function Register() {
   const [step, setStep] = useState(1);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const canGoNext = () => {
+    if (step === 1) return firstName.trim() !== "" && lastName.trim() !== "";
+    if (step === 2) return email.trim() !== "" && password.trim() !== "";
+    return true;
+  };
+
   return (
     <div className="min-h-screen w-full bg-gray-100 flex items-center justify-center">
       <div className="bg-white rounded-2xl flex flex-row w-335 h-180 p-4">
@@ -26,21 +38,33 @@ export default function Register() {
                 <>
                   <div className="flex flex-col gap-1">
                     <label className="text-sm text-black font-medium">
-                      First Name
+                      First Name{" "}
+                      <span className="text-orange-600 text-lg">*</span>
                     </label>
                     <input
                       type="text"
-                      className="border border-gray-200 rounded-lg px-4 py-3 w-full outline-none focus:border-gray-400"
+                      value={firstName}
+                      onChange={(e) => setFirstName(e.target.value)}
+                      className="border text-black border-gray-200 rounded-lg px-4 py-3 w-full outline-none focus:border-gray-400"
                     />
                   </div>
                   <div className="flex flex-col gap-1">
                     <label className="text-sm text-black font-medium">
-                      Last Name
+                      Last Name{" "}
+                      <span className="text-orange-600 text-lg">*</span>
                     </label>
                     <input
                       type="text"
-                      className="border border-gray-200 rounded-lg px-4 py-3 w-full outline-none focus:border-gray-400"
+                      value={lastName}
+                      onChange={(e) => setLastName(e.target.value)}
+                      className="border text-black border-gray-200 rounded-lg px-4 py-3 w-full outline-none focus:border-gray-400"
                     />
+                    <Link
+                      href="/login"
+                      className="text-gray-400 hover:underline text-center text-sm mt-1"
+                    >
+                      Already have an account ?
+                    </Link>
                   </div>
                 </>
               )}
@@ -49,21 +73,32 @@ export default function Register() {
                 <>
                   <div className="flex flex-col gap-1">
                     <label className="text-sm text-black font-medium">
-                      Email
+                      Email <span className="text-orange-600 text-lg">*</span>
                     </label>
                     <input
                       type="email"
-                      className="border border-gray-200 rounded-lg px-4 py-3 w-full outline-none focus:border-gray-400"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      className="border text-black border-gray-200 rounded-lg px-4 py-3 w-full outline-none focus:border-gray-400"
                     />
                   </div>
                   <div className="flex flex-col gap-1">
                     <label className="text-sm text-black font-medium">
-                      Password
+                      Password{" "}
+                      <span className="text-orange-600 text-lg">*</span>
                     </label>
                     <input
                       type="password"
-                      className="border border-gray-200 rounded-lg px-4 py-3 w-full outline-none focus:border-gray-400"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      className="border text-black border-gray-200 rounded-lg px-4 py-3 w-full outline-none focus:border-gray-400"
                     />
+                    <Link
+                      href="/login"
+                      className="text-gray-400 hover:underline text-center text-sm mt-1"
+                    >
+                      Already have an account ?
+                    </Link>
                   </div>
                 </>
               )}
@@ -76,7 +111,7 @@ export default function Register() {
                     </label>
                     <input
                       type="text"
-                      className="border border-gray-200 rounded-lg px-4 py-3 w-full outline-none focus:border-gray-400"
+                      className="border text-black border-gray-200 rounded-lg px-4 py-3 w-full outline-none focus:border-gray-400"
                     />
                   </div>
                   <div className="flex flex-col gap-1">
@@ -85,7 +120,7 @@ export default function Register() {
                     </label>
                     <input
                       type="date"
-                      className="border border-gray-200 rounded-lg px-4 py-3 w-full outline-none focus:border-gray-400"
+                      className="border text-black border-gray-200 rounded-lg px-4 py-3 w-full outline-none focus:border-gray-400"
                     />
                   </div>
                 </>
@@ -98,7 +133,7 @@ export default function Register() {
                       About me
                     </label>
                     <textarea
-                      className="border border-gray-200 rounded-lg px-4 py-3 w-full outline-none focus:border-gray-400 resize-none"
+                      className="border text-black border-gray-200 rounded-lg px-4 py-3 w-full outline-none focus:border-gray-400 resize-none"
                       rows={3}
                     />
                   </div>
@@ -117,18 +152,30 @@ export default function Register() {
           </div>
 
           <div className="flex flex-row justify-between">
-            <button
-              onClick={() => setStep(step - 1)}
-              className="cursor-pointer bg-black text-white px-3 py-2 rounded-full font-medium hover:bg-zinc-800 transition-colors"
-            >
-              <FaArrowLeft />
-            </button>
-            <button
-              onClick={() => setStep(step + 1)}
-              className="cursor-pointer bg-black text-white px-3 py-2 rounded-full font-medium hover:bg-zinc-800 transition-colors"
-            >
-              <FaArrowRight />
-            </button>
+            {step > 1 && (
+              <button
+                onClick={() => setStep(step - 1)}
+                className="bg-black cursor-pointer text-white px-3 py-2 rounded-full font-medium hover:bg-zinc-800 transition-colors"
+              >
+                <FaArrowLeft />
+              </button>
+            )}
+
+            {step < 4 && (
+              <button
+                onClick={() => setStep(step + 1)}
+                disabled={!canGoNext()}
+                className="bg-black cursor-pointer text-white px-3 py-2 rounded-full font-medium hover:bg-zinc-800 transition-colors ml-auto disabled:opacity-40 disabled:cursor-not-allowed"
+              >
+                <FaArrowRight />
+              </button>
+            )}
+
+            {step === 4 && (
+              <button className="bg-black cursor-pointer text-white px-6 py-2 rounded-full font-medium hover:bg-zinc-800 transition-colors">
+                Join us
+              </button>
+            )}
           </div>
         </div>
       </div>

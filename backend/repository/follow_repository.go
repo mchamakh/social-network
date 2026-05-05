c4ad9f2986d4cda5c9814c8088290bf50b0f03be
clegand <corentinlgnd@gmail.com>
2026-05-05 10:27:46 +0200

    add follow repo

---
+++ backend/repository/follow_repository.go
@@ -0,0 +1,44 @@
+package repository
+
+import (
+	"api/model"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type followRepository struct {
+	db *gorm.DB
+}
+
+func NewFollowRepository(db *gorm.DB) model.FollowRepository {
+	return &followRepository{db: db}
+}
+
+func (r *followRepository) Create(f *model.Follow) error {
+	return r.db.Create(f).Error
+}
+
+func (r *followRepository) Get(followerID, followingID uuid.UUID) (*model.Follow, error) {
+	var follow model.Follow
+	err := r.db.Where("follower_id = ? AND following_id = ?", followerID, followingID).First(&follow).Error
+	if err != nil {
+		return nil, err
+	}
+	return &follow, err
+}
+
+func (r *followRepository) UpdateStatus(id uuid.UUID, status model.FollowStatus) error {
+	return r.db.Model(&model.Follow{}).Where("id = ?", id).Update("status", status).Error
+}
+
+func (r *followRepository) Delete(id uuid.UUID) error {
+	return r.db.Delete(&model.Follow{}, "id = ?", id).Error
+}
+
+func (r *followRepository) GetPendingRequests(userID uuid.UUID) ([]model.Follow, error) {
+	var follows []model.Follow
+	err := r.db.Where("following_id = ? AND status = ?", userID, "pending").Find(&follows).Error
+
+	return follows, err
+}

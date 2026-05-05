a8b3f99597e670be16d81a5e52e664c1a23fd718
clegand <corentinlgnd@gmail.com>
2026-05-05 12:31:39 +0200

    add follow logic

--- backend/container/container.go
+++ backend/container/container.go
@@ -9,22 +9,27 @@ import (
 )
 
 type Container struct {
-	UserHandler *handler.UserHandler
-	AuthHandler *handler.AuthHandler
+	UserHandler   *handler.UserHandler
+	AuthHandler   *handler.AuthHandler
+	FollowHandler *handler.FollowHandler
 }
 
 func NewContainer(db *gorm.DB) *Container {
 	userRepo := repository.NewUserRepository(db)
 	refreshRepo := repository.NewRefreshTokenRepository(db)
+	followRepo := repository.NewFollowRepository(db)
 
 	userService := service.NewUserService(userRepo)
 	authService := service.NewAuthService(userService, refreshRepo)
+	followService := service.NewFollowService(followRepo, userRepo)
 
 	userHandler := handler.NewUserHandler(userService)
 	authHandler := handler.NewAuthHandler(authService)
+	followHandler := handler.NewFollowHandler(followService)
 
 	return &Container{
-		UserHandler: userHandler,
-		AuthHandler: authHandler,
+		UserHandler:   userHandler,
+		AuthHandler:   authHandler,
+		FollowHandler: followHandler,
 	}
 }
